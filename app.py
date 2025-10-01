02bb2e3b7fc06d89d3e53af6e70db04b6fb62b58
sasida dilhan <it21158254@my.sliit.lk>
2025-10-01 21:52:47 +0530

    backen logic created

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        ram = request.form['ram']
+        weight = request.form['weight']
+        company = request.form['company']
+        typename = request.form['typename']
+        opsys = request.form['opsys']
+        cpuname = request.form['cpuname']
+        gpuname = request.form['gpuname']
+        touchscreen = request.form.getlist('touchscreen')
+        ips = request.form.getlist('ips')
+
+        feature_list = []
+        feature_list.append(int(ram))
+        feature_list.append(float(weight))
+        feature_list.append(len(touchscreen))
+        feature_list.append(len(ips))
+        
+        company_list = ['acer', 'apple', 'asus', 'dell', 'hp', 'lenovo', 'msi', 'other', 'toshiba']
+        typename_list = ['2in1convertible', 'gaming', 'netbook', 'notebook', 'ultrabook', 'workstation']
+        opsys_list = ['linux', 'mac', 'other', 'windows']
+        cpu_list = ['amd', 'intelcorei3', 'intelcorei5', 'intelcorei7', 'other']
+        gpu_list = ['amd', 'intel', 'nvidia']
+
+
+        def traves(list, value):
+            for item in list:
+                if item == value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+
+        traves(company_list, company)
+        traves(typename_list, typename)
+        traves(opsys_list, opsys)
+        traves(cpu_list, cpuname)
+        traves(gpu_list, gpuname)
+        print(feature_list)
+
+
     return render_template('index.html')
 
 if __name__ == '__main__':
